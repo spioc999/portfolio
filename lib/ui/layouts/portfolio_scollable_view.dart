67565efac262c364c2d9a6590fbbd65605ee61bd
Simone Pio Caronia <spcaronia@gmail.com>
2024-05-29 01:08:22 +0200

    Create portfolio body, start new pages and fitted box for titles

--- lib/ui/layouts/portfolio_scollable_view.dart
+++ lib/ui/layouts/portfolio_scollable_view.dart
@@ -6,8 +6,35 @@ import 'package:spioc_portfolio/ui/components/app_divider.dart';
 import 'package:spioc_portfolio/utils/extensions.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class PortfolioSliverFooter extends StatelessWidget {
-  const PortfolioSliverFooter({super.key});
+class PortfolioScrollableView extends StatelessWidget {
+  final Widget body;
+  final bool hasFooter;
+
+  const PortfolioScrollableView({
+    super.key,
+    required this.body,
+    this.hasFooter = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      physics: defaultScrollPhysics,
+      slivers: [
+        SliverPadding(
+          padding: defaultBodyPadding,
+          sliver: SliverToBoxAdapter(
+            child: body,
+          ),
+        ),
+        if (hasFooter) const _PortfolioSliverFooter(),
+      ],
+    );
+  }
+}
+
+class _PortfolioSliverFooter extends StatelessWidget {
+  const _PortfolioSliverFooter();
 
   @override
   Widget build(BuildContext context) {
