b3e5a204a6455d35ee5713c04384cdd9cb2da5da
Simone Pio Caronia <spcaronia@gmail.com>
2024-09-02 20:53:43 +0200

    fix ui and bump version

--- lib/views/experience/_experience_item.dart
+++ lib/views/experience/_experience_item.dart
@@ -6,6 +6,7 @@ import 'package:spioc_portfolio/ui/components/app_markdown.dart';
 import 'package:spioc_portfolio/ui/components/at_place_clickable.dart';
 import 'package:spioc_portfolio/ui/components/logo_squared_network_image.dart';
 import 'package:spioc_portfolio/ui/layouts/portfolio_time_bounded_layout.dart';
+import 'package:spioc_portfolio/ui/layouts/row_or_column_layout.dart';
 import 'package:spioc_portfolio/utils/extensions.dart';
 import 'package:spioc_portfolio/utils/responsive_values.dart';
 
@@ -26,68 +27,63 @@ class ExperienceItem extends StatelessWidget {
       child: PortfolioTimeBoundedLayout(
         startDateTime: experience.startDateTime,
         endDateTime: experience.endDateTime,
-        child: Row(
+        child: RowOrColumnLayout(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            LogoSquaredNetworkImage.experience(
-              imgUrl: experience.companyImageUrl ?? emptyString,
-            ),
-            defaultMarginGap,
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    experience.role(l10n(context)).toUpperCase(),
-                    style: ResponsiveValues.themeTitleStyle(context)?.copyWith(
-                      height: Dimens.one,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  smallMarginGap,
-                  AtPlaceClickable(
-                    place: experience.company,
-                    link: experience.companyUrl,
-                    onLinkTap: onLinkTap,
-                    style: ResponsiveValues.themeBodyStyle(context),
-                  ),
-                  if (experience.locality != null ||
-                      experience.workingType != null) ...[
-                    verySmallMarginGap,
-                    Text.rich(
-                      TextSpan(
-                        style: ResponsiveValues.themeLabelStyle(context),
-                        children: [
-                          if (experience.locality != null)
-                            TextSpan(
-                              text: experience.locality!(l10n(context))
-                                  .toUpperCase(),
-                            ),
-                          if (experience.locality != null &&
-                              experience.workingType != null)
-                            const TextSpan(
-                              text: dashWithSpacingString,
-                            ),
-                          if (experience.workingType != null)
-                            TextSpan(
-                              text: experience.workingType!
-                                  .label(l10n(context))
-                                  .toUpperCase(),
-                            ),
-                        ],
-                      ),
-                    ),
-                  ],
-                  if (experience.description != null) ...[
-                    smallMarginGap,
-                    AppMarkdown(
-                      data: experience.description!(l10n(context)),
-                    ),
-                  ],
-                ],
+          first: LogoSquaredNetworkImage.experience(
+            imgUrl: experience.companyImageUrl ?? emptyString,
+          ),
+          second: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                experience.role(l10n(context)).toUpperCase(),
+                style: ResponsiveValues.themeTitleStyle(context)?.copyWith(
+                  height: Dimens.one,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          ],
+              smallMarginGap,
+              AtPlaceClickable(
+                place: experience.company,
+                link: experience.companyUrl,
+                onLinkTap: onLinkTap,
+                style: ResponsiveValues.themeBodyStyle(context),
+              ),
+              if (experience.locality != null ||
+                  experience.workingType != null) ...[
+                verySmallMarginGap,
+                Text.rich(
+                  TextSpan(
+                    style: ResponsiveValues.themeLabelStyle(context),
+                    children: [
+                      if (experience.locality != null)
+                        TextSpan(
+                          text:
+                              experience.locality!(l10n(context)).toUpperCase(),
+                        ),
+                      if (experience.locality != null &&
+                          experience.workingType != null)
+                        const TextSpan(
+                          text: dashWithSpacingString,
+                        ),
+                      if (experience.workingType != null)
+                        TextSpan(
+                          text: experience.workingType!
+                              .label(l10n(context))
+                              .toUpperCase(),
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+              if (experience.description != null) ...[
+                smallMarginGap,
+                AppMarkdown(
+                  data: experience.description!(l10n(context)),
+                ),
+              ],
+            ],
+          ),
         ),
       ),
     );
