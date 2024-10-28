5507964b4a5dbf0ae02d11a7d299342453d98701
Simone Pio Caronia <simpio99@libero.it>
2024-10-28 12:42:00 +0100

    Fix title and name

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:spioc_portfolio/app/portfolio_web_app.dart';
+import 'package:spioc_portfolio/constants/common.dart';
 import 'package:spioc_portfolio/core/locator/locator.dart';
 import 'package:spioc_portfolio/core/routing/routing_config.dart';
+import 'package:spioc_portfolio/utils/web_utils.dart';
 
 void main() async {
+  WebUtils.setTitle(loading);
+
   await commonRuns();
 
   runApp(const PortfolioWebApp());
