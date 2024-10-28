ea06b518afa3f9c9620150e3ffb2c3682479702e
Simone Pio Caronia <simpio99@libero.it>
2024-10-28 12:41:56 +0100

    Fix observer and name

--- lib/core/routing/routing_observers.dart
+++ lib/core/routing/routing_observers.dart
@@ -1,34 +1,32 @@
 import 'package:collection/collection.dart';
 import 'package:flutter/widgets.dart';
 import 'package:spioc_portfolio/constants/common.dart';
-import 'package:spioc_portfolio/core/routing/route_names.dart';
+import 'package:spioc_portfolio/core/routing/route_name.dart';
 import 'package:spioc_portfolio/core/routing/routing_config.dart';
 import 'package:spioc_portfolio/utils/web_utils.dart';
 
 class WebAppTitleNavigationObserver extends NavigatorObserver {
   @override
   void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
-    _setWebAppTitle(route.settings.name, route.settings.arguments);
+    _setWebAppTitle(route.settings.name);
   }
 
   @override
   void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
-    _setWebAppTitle(
-        previousRoute?.settings.name, previousRoute?.settings.arguments);
+    _setWebAppTitle(previousRoute?.settings.name);
   }
 
   @override
   void didRemove(Route<dynamic> route, Route<dynamic>? previousRoute) {
-    _setWebAppTitle(
-        previousRoute?.settings.name, previousRoute?.settings.arguments);
+    _setWebAppTitle(previousRoute?.settings.name);
   }
 
   @override
   void didReplace({Route<dynamic>? newRoute, Route<dynamic>? oldRoute}) {
-    _setWebAppTitle(newRoute?.settings.name, newRoute?.settings.arguments);
+    _setWebAppTitle(newRoute?.settings.name);
   }
 
-  void _setWebAppTitle(String? name, [dynamic arguments]) {
+  void _setWebAppTitle(String? name) {
     final routeEnum = RouteName.values.firstWhereOrNull((r) => r.name == name);
 
     if (routeEnum == null) return;
